4ecebcaa67725f5acd0cf9900401022f2a13ae3c
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-06-07 23:58:32 +1000

    Create Activity and API classes

--- RestfulAPIClient/app/src/main/java/com/giao/restfulapiclient/MainActivity.java
+++ RestfulAPIClient/app/src/main/java/com/giao/restfulapiclient/MainActivity.java
@@ -1,13 +1,49 @@
 package com.giao.restfulapiclient;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String listItem []={"Login","Create User Account","Department  Details"};
+        ListView lvMain=(ListView)findViewById(R.id.lv_main);
+        lvMain.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listItem));
+
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+
+                if(arg2==0)
+                {
+                    Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(i);
+                }
+                else if(arg2==1)
+                {
+                    Intent i=new Intent(MainActivity.this,CreateUserActivity.class);
+                    startActivity(i);
+                }
+                else if(arg2==2)
+                {
+                    Intent i=new Intent(MainActivity.this,DeptActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
     }
+
+ 
+
 }
---
+++ RestfulAPIClient/app/src/main/java/com/giao/restfulapiclient/UserDetailsTable.java
@@ -0,0 +1,60 @@
+package com.giao.restfulapiclient;
+
+/**
+ * Created by Long on 6/7/2016.
+ */
+public class UserDetailsTable {
+
+    String firstName,lastName,userName,password;
+
+    public UserDetailsTable(String firstName, String lastName, String userName,
+                            String password) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public UserDetailsTable() {
+        super();
+        this.firstName = null;
+        this.lastName = null;
+        this.userName = null;
+        this.password = null;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
